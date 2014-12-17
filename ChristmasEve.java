161a8b5f4ff50f5009396c688da1597922096b4b
Chorryi <lennestir@gmail.com>
2014-12-17 11:01:18 -0500

    Update ChristmasEve.java

--- ChristmasEve.java
+++ ChristmasEve.java
@@ -1,10 +1,27 @@
+
 /**
- * Write a description of class ChristmasEve here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * This will draw Christmas night
+ * The night with the stars
+ * The sleigh lying on the house
+ * The north star
  */
-public class ChristmasEve
+
+import java.awt.*;
+import java.applet.*;
+import java.util.*;
+
+public class ChristmasEve extends Applet
 {
+    public ChristmasEve(Graphics g)
+    {
+        drawChristmasEve(g);
+    }
     
+    public void drawChristmasEve(Graphics g)
+    {
+        g.setColor(new Color(0, 51, 102));
+        g.fillRect(0,0,1920,1080);
+        g.setColor(new Color(237, 244, 245));
+        g.fillRect(0,820,1920,260);
+    }
 }
