48153fef00244b0cbd9e0ee34be2d38b38a87eb8
Chorryi <lennestir@gmail.com>
2014-12-22 09:51:59 -0500

    Update ChristmasEve.java

--- ChristmasEve.java
+++ ChristmasEve.java
@@ -1,5 +1,4 @@
 
-
 /**
  * This will draw Christmas night
  * The night with the stars
@@ -11,18 +10,21 @@ import java.awt.*;
 import java.applet.*;
 import java.util.*;
 
-public class ChristmasEve extends Applet
+class ChristmasEve extends Applet
 {
     public ChristmasEve(Graphics g)
     {
-        drawNight(g);
+        drawScene(g);
     }
-    class Night extends ChristmasEve
+    public void Scene
+    {
+    }
+    class Night extends Scene
     {
         g.setColor(new Color(0, 51, 102));
         g.fillRect(0,0,1920,1080);
     }
-    class Snow extends ChristmasEve
+    class Snow extends Scene
     {
         g.fillRect(0,820,1920,260);
         g.setColor(new Color(237, 244, 245));
