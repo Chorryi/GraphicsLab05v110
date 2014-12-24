790b620093cfb2224616481832b387b341e3bbd1
Chorryi <lennestir@gmail.com>
2014-12-23 18:37:37 -0800

    Create Windows.java

---
+++ Windows.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+import java.applet.*;
+import java.util.*; 
+						
+class Windows
+{
+   public Windows (Graphics g)
+   {
+   g.setColor(Color.white);
+   g.fillRect(510,185,60,60);
+   g.setColor(Color.black);
+   g.drawLine(540,185,540,245);
+   g.drawLine(510,215,570,215); 
+     
+   g.setColor(Color.white);
+   g.fillRect(510,290,60,60);
+   g.setColor(Color.black);
+   g.drawLine(540,290,540,350);
+   g.drawLine(510,320,570,320); 
+   
+   g.setColor(Color.white);
+   g.fillRect(510,395,60,60);
+   g.setColor(Color.black);
+   g.drawLine(540,395,540,455);
+   g.drawLine(510,425,570,425);
+   
+   g.setColor(Color.white);
+   g.fillRect(510,500,60,60);
+   g.setColor(Color.black);
+   g.drawLine(540,500,540,560);
+   g.drawLine(510,530,570,530);
+
+   
+   g.setColor(Color.white);
+   g.fillRect(710,185,60,60);
+   g.setColor(Color.black);
+   g.drawLine(740,185,740,245);
+   g.drawLine(710,215,770,215);
+   
+   g.setColor(Color.white);
+   g.fillRect(710,290,60,60);
+   g.setColor(Color.black);
+   g.drawLine(740,290,740,350);
+   g.drawLine(710,320,770,320);
+   
+   g.setColor(Color.white);
+   g.fillRect(710,395,60,60);
+   g.setColor(Color.black);
+   g.drawLine(740,395,740,455);
+   g.drawLine(710,425,770,425);
+   
+   g.setColor(Color.white);
+   g.fillRect(710,500,60,60);
+   g.setColor(Color.black);
+   g.drawLine(740,500,740,560);
+   g.drawLine(710,530,770,530);
+   }
+}
